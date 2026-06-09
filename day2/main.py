ef3042cc03d44f3308d29a175cd3ced2108aceb0
Harsh-Rawat1706 <hr9785771@gmail.com>
2026-06-09 13:56:19 +0530

    day2-path-parameters

---
+++ day2/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+students = dict({1: {"name": "Harsh", "age": 20, "course": "python"},
+                 2: {"name": "Satyarth", "age": 21, "course": "java"},
+                 3: {"name": "Satyam", "age": 22, "course": "c++"}})
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to the Student Course API!"}
+@app.get("/get_students/{student_id}")
+def get_students(student_id: int):
+    return {"student": students.get(student_id)}
+
+@app.post("/add_student/{name}/{age}/{course}")
+def add_student(name: str, age: int, course: str):
+    student = dict({"name": name, "age": age, "course": course})
+    student_id = len(students) + 1
+    students[student_id] = student
+    return {"message": f"Student added successfully!", "student_id": student_id}
+
+@app.delete("/delete_student/{student_id}")
+def delete_student(student_id: int):
+    if student_id in students:
+        del students[student_id]
+        return {"message": f"Student '{student_id}' removed successfully!"}
+    else:
+        return {"message": f"Student '{student_id}' not found!"}
