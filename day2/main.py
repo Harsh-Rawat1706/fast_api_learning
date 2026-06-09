b924f416ad48fca2a199844dab501a02389defe6
Harsh-Rawat1706 <hr9785771@gmail.com>
2026-06-09 20:38:34 +0530

    path function and http exceptions

--- day2/main.py
+++ day2/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi import Path , HTTPException
 
 app = FastAPI()
 
@@ -10,8 +11,10 @@ students = dict({1: {"name": "Harsh", "age": 20, "course": "python"},
 def home():
     return {"message": "Welcome to the Student Course API!"}
 @app.get("/get_students/{student_id}")
-def get_students(student_id: int):
-    return {"student": students.get(student_id)}
+def get_students(student_id: int = Path(..., description="The ID of the student to retrieve", example=1)):
+    if student_id in students:
+        return students[student_id]
+    raise HTTPException(status_code=404, detail=f"Student with ID {student_id} not found")
 
 @app.post("/add_student/{name}/{age}/{course}")
 def add_student(name: str, age: int, course: str):
@@ -21,9 +24,8 @@ def add_student(name: str, age: int, course: str):
     return {"message": f"Student added successfully!", "student_id": student_id}
 
 @app.delete("/delete_student/{student_id}")
-def delete_student(student_id: int):
+def delete_student(student_id: int = Path(..., description="The ID of the student to delete", example=1)):
     if student_id in students:
         del students[student_id]
         return {"message": f"Student '{student_id}' removed successfully!"}
-    else:
-        return {"message": f"Student '{student_id}' not found!"}
+    raise HTTPException(status_code=404, detail=f"Student with ID {student_id} not found")
