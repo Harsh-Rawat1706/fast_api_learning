387f3349cd4c4167fa94bbe7ef555a39edb5a238
Harsh-Rawat1706 <hr9785771@gmail.com>
2026-06-10 20:52:04 +0530

    annotated_field_computed

--- assignment_day3/main.py
+++ assignment_day3/main.py
@@ -1,31 +1,44 @@
 from fastapi import FastAPI
 from fastapi import Path , HTTPException ,Query
-from pydantic import BaseModel
-from typing import Optional
+from pydantic import BaseModel , Field , computed_field
+from typing import Optional , Annotated , Literal
 
 app = FastAPI()
 
+#Temporary Database 
 students = []
 teachers = []
 courses = []
 
 class Student(BaseModel):
-    name: str
-    age: int
-    course: str = "Data Science"  # default value for course is set to "Data Science"
-    cgpa: float
+    name: Annotated[str, Field(...,description="The name of the student", example="John Doe")]
+    age: Annotated[int, Field(...,gt = 0,lt = 120,description="The age of the student", example=20)]
+    course: Annotated[str, Field(..., description="The course the student is enrolled in", example="Data Science")]
+    cgpa: Annotated[float, Field(...,ge=0.0,le=10.0, description="The CGPA of the student", example=8.5)]
     phone: Optional[str] = None  # Optional field for phone number
-    
+    @computed_field 
+    @property
+    def Grade(self) -> str:
+        if self.cgpa >= 9.0:
+            return "A"
+        elif self.cgpa >= 8.0:
+            return "B"
+        elif self.cgpa >= 7.0:
+            return "C"
+        elif self.cgpa >= 6.0:
+            return "D"
+        else:
+            return "F"
 class Teacher(BaseModel):
-    name: str
-    age: int
-    subject: str
-    experience: int  # years of experience
+    name: Annotated[str, Field(...,description="The name of the teacher", example="Jane Smith")]
+    age: Annotated[int, Field(...,gt = 0,lt = 120,description="The age of the teacher", example=35)]
+    subject: Annotated[str, Field(...,description="The subject the teacher teaches", example="Mathematics")]
+    experience: Annotated[int, Field(...,ge=0,description="The years of experience the teacher has", example=10)]  # years of experience
 
 class Course(BaseModel):
-    name: str
-    teacher: str
-    duration: int  # duration in weeks
+    name: Annotated[str, Field(...,description="The name of the course", example="Introduction to Python")]
+    teacher: Annotated[str, Field(...,description="The teacher of the course", example="Jane Smith")]
+    duration: Annotated[int, Field(...,gt=0,description="The duration of the course in weeks", example=12)]  # duration in weeks
     
 @app.get("/")
 def home():
