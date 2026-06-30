1d09228805f81d86e87688f70773d9d5b4e187e1
Harsh-Rawat1706 <hr9785771@gmail.com>
2026-06-30 13:08:41 +0530

    commit

--- college_management_system/app/models/teacher.py
+++ college_management_system/app/models/teacher.py
@@ -3,10 +3,16 @@ from uuid import UUID
 
 from sqlalchemy import Date, DateTime, ForeignKey, String, func, text
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import relationship
 
 from app.db.database import Base
 
+from typing import TYPE_CHECKING
 
+if TYPE_CHECKING:
+    from app.models.department import Department
+    from app.models.user import User
+    from app.models.course import Course
 class Teacher(Base):
     __tablename__ = "teachers"
 
@@ -20,12 +26,20 @@ class Teacher(Base):
         unique=True,
         nullable=False
     )
-
+    
+    user: Mapped["User"] = relationship(
+    back_populates="teacher"
+    )
+    
     department_id: Mapped[UUID] = mapped_column(
         ForeignKey("departments.id"),
         nullable=False
     )
-
+    
+    department: Mapped["Department"] = relationship(
+    back_populates="teachers"
+    )
+    
     employee_code: Mapped[str] = mapped_column(
         String(15),
         unique=True,
@@ -46,4 +60,8 @@ class Teacher(Base):
         DateTime(timezone=True),
         server_default=func.now(),
         nullable=False
+    )
+    
+    courses: Mapped[list["Course"]] = relationship(
+    back_populates="teacher"
     )
